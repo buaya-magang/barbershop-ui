6dad55191df7e288588bcd11772738e529004690
buayaMAGANG <buayamagang13@gmail.com>
2025-08-14 17:46:11 +0700

    feat: use netlify.toml for SPA redirects

--- svelte.config.js
+++ svelte.config.js
@@ -2,11 +2,8 @@ import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter(),
-    prerender: {
-      entries: ['*'] // prerender semua halaman
-    }
-  }
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter()
+	}
 };
