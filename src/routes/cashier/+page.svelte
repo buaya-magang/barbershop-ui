829adaf52dcff4ecde1d6ee0fc41f08cd3afc118
buayaMAGANG <buayamagang13@gmail.com>
2025-08-18 13:19:12 +0700

    responsive

--- src/routes/cashier/+page.svelte
+++ src/routes/cashier/+page.svelte
@@ -3,12 +3,12 @@
   import TransactionCart from '$lib/components/TransactionCart.svelte';
   import type { PageData } from './$types';
 
-  // Menerima 'data' yang dikirim dari load function di +page.ts
   export let data: PageData;
 </script>
 
-<div class="flex h-full w-full flex-row gap-6">
+<div class="flex h-full w-full flex-col lg:flex-row gap-6">
   
+  <!-- Kolom Produk -->
   <div class="flex flex-grow flex-col gap-4">
     
     <div class="flex-shrink-0">
@@ -25,7 +25,8 @@
 
   </div>
 
-  <aside class="w-[350px] flex-shrink-0">
+  <!-- Sidebar / Cart -->
+  <aside class="w-full lg:w-[350px] flex-shrink-0">
     <TransactionCart />
   </aside>
 
