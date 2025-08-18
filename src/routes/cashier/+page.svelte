71c90957b8369bce7878c75f93c49c357eac5465
buayaMAGANG <buayamagang13@gmail.com>
2025-08-18 13:37:43 +0700

    Ganti pr

--- src/routes/cashier/+page.svelte
+++ src/routes/cashier/+page.svelte
@@ -17,18 +17,15 @@
   <!-- Kolom Produk -->
   <div class="flex flex-grow flex-col gap-4">
     
-    <!-- Search -->
-    <div class="flex-shrink-0 px-2 lg:px-0">
+    <!-- Bungkus search + grid biar sejajar -->
+    <div class="flex flex-col gap-4">
       <input
         type="text"
         placeholder="Cari produk atau layanan..."
         bind:value={searchQuery}
         class="w-full rounded-lg border border-slate-300 bg-white p-4"
       />
-    </div>
-    
-    <!-- Grid Produk -->
-    <div class="flex-grow overflow-y-auto px-2 lg:px-0">
+
       <ProductGrid items={filteredItems} />
     </div>
 
