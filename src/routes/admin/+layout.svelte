4f9649b9cc163efab2c2ebedb08763a599e2c6ed
buayaMAGANG <buayamagang13@gmail.com>
2025-08-20 11:06:40 +0700

    fix: admin dashboard active

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -37,13 +37,13 @@
       <!-- Menu Admin -->
       <nav class="hidden items-center gap-6 md:flex">
         <a 
-          href="/admin" 
-          class="pb-2 font-semibold transition"
-          class:border-b-2={$page.url.pathname.startsWith('/admin')}
-          class:border-violet-600={$page.url.pathname.startsWith('/admin')}
-          class:text-violet-600={$page.url.pathname.startsWith('/admin')}
-          class:text-slate-500={!$page.url.pathname.startsWith('/admin')}
-          class:hover:text-violet-600={!$page.url.pathname.startsWith('/admin')}
+        href="/admin" 
+        class="pb-2 font-semibold transition"
+        class:border-b-2={$page.url.pathname === '/admin'}
+        class:border-violet-600={$page.url.pathname === '/admin'}
+        class:text-violet-600={$page.url.pathname === '/admin'}
+        class:text-slate-500={$page.url.pathname !== '/admin'}
+        class:hover:text-violet-600={$page.url.pathname !== '/admin'}
         >
         Dashboard
         </a>
